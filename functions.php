aad7840e0190cc6a211aa7c31ebdb444e4dc8501
WinterCricket <hodhodhod2003@gmail.com>
2020-03-03 00:37:13 -0600

    break and repair

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
 function bridgestreet_files() {
-  
+	wp_enqueue_style('google-style', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('bridgestreet_styles', get_stylesheet_uri(), NULL, microtime());
 }
 
